9ea1fd6c819dbeb56841fbea35f64f66ca9fc34d
shgonzals <sh.gonzal.s@gmail.com>
2024-03-14 18:44:21 +0100

    first commit

---
+++ app/src/main/java/com/example/qrgenerator/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.qrgenerator;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.qrgenerator.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://qrgen.shgonzals.es/");
+
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+}
